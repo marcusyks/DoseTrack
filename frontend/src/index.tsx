a01a4a5e27a997dc85b1ddc4551b6da1b3413857
Marcus <marcus.yks27@gmail.com>
2024-03-17 21:51:23 +0800

    Commit

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -19,7 +19,7 @@ root.render(
       scope:"profile email read:current_user delete:current_user update:current_user_metadata"
     }}
   >
-    <BrowserRouter basename="https://dose-track-nu.vercel.app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Auth0Provider>
