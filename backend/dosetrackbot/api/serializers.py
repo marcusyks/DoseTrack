cd96f90d71c1a0c0de609a6f83b01dbcc1da25e3
marcusyks <marcus.yks27@gmail.com>
2024-03-31 16:46:26 +0800

    MVP v1:

--- backend/dosetrackbot/api/serializers.py
+++ backend/dosetrackbot/api/serializers.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework.serializers import ModelSerializer
 from django.http import JsonResponse
 from ..models import TelegramState
@@ -6,3 +7,17 @@ class TelegramStateSerializer(ModelSerializer):
     class Meta:
         model = TelegramState
         fields = ["id","memory","name","telegram_chat_id","telegram_user_id"]
+=======
+from rest_framework.serializers import ModelSerializer
+from django.http import JsonResponse
+from ..models import TelegramState
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class TelegramStateSerializer(ModelSerializer):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    class Meta:
+        model = TelegramState
+        fields = ["id","memory","name","telegram_chat_id","telegram_user_id"]
+>>>>>>> 78e0a92 (MVP v1:)
