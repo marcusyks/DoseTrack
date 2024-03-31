7407bd4c363e9fbc579756ad925af17bf6dbc7c1
marcusyks <marcus.yks27@gmail.com>
2024-03-31 15:21:42 +0800

    MVP v1:

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,5 +4,10 @@ python3.9 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# Test redis
+sudo apt-get update
+sudo apt install redis-server
+redis-cli ping
+
 # Starts celery
 celery -A core worker -l INFO --beat
