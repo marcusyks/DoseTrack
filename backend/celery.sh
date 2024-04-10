13859721e26e2a704c531a3be8a6681c8fa8b05a
Marcus <marcus.yks27@gmail.com>
2024-04-10 22:43:30 +0800

    MVP v1.3

--- backend/celery.sh
+++ backend/celery.sh
@@ -4,5 +4,4 @@ CELERY_LOG_DIR="/var/log/celery"
 
 mkdir -p "$CELERY_RUN_DIR" "$CELERY_LOG_DIR"
 chown -R nobody:nogroup "$CELERY_RUN_DIR" "$CELERY_LOG_DIR"
-
 celery -A core worker --loglevel=info --beat --pidfile="$CELERY_RUN_DIR/%n.pid" --logfile="$CELERY_LOG_DIR/%n%I.log"
