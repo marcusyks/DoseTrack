987013d57a08f390e28f4406b627f1b14570a7cf
Marcus <marcus.yks27@gmail.com>
2024-04-10 23:21:13 +0800

    MVP Working

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,10 +20,6 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-APP_NAME= os.environ.get("FLY_APP_NAME")
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
