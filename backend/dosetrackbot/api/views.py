e1b49ad71ec4b014e422fe6140994c50c8742b38
Marcus <marcus.yks27@gmail.com>
2024-04-11 00:05:55 +0800

    MVP v1

--- backend/dosetrackbot/api/views.py
+++ backend/dosetrackbot/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-from .models import TelegramState
+from ..models import TelegramState
 from .serializers import TelegramStateSerializer
 
 class TelegramStateViewSet(ModelViewSet):
