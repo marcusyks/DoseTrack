63bb16b908856706629b86c406ed11874ea59e6a
Marcus <marcus.yks27@gmail.com>
2024-04-10 23:21:13 +0800

    MVP Working

--- backend/plans/api/views.py
+++ backend/plans/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from ..models import Plan, Medicine
 from .serializers import PlanSerializer, MedicineSerializer
+from rest_framework.authentication import SessionAuthentication
 
 
 class PlanViewSet(ModelViewSet):
