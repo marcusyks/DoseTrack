4e11e4d409fd24ce44307e72e796cb4d54ba9742
Marcus <marcus.yks27@gmail.com>
2024-04-11 00:05:55 +0800

    MVP v1

--- backend/dosetrackbot/api/views.py
+++ backend/dosetrackbot/api/views.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from rest_framework.response import Response
@@ -20,3 +21,15 @@ class TelegramStateViewSet(ModelViewSet):
     serializer_class = TelegramStateSerializer
     permission_classes = [IsAuthenticated]
 >>>>>>> 2b6b674 (MVP)
+=======
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from ..models import TelegramState
+from .serializers import TelegramStateSerializer
+
+class TelegramStateViewSet(ModelViewSet):
+    queryset = TelegramState.objects.all()
+    serializer_class = TelegramStateSerializer
+    permission_classes = [IsAuthenticated]
+>>>>>>> b2c07b6 (MVP v1)
