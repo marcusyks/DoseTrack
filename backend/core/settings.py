ee7e83026f33b4cfdeceb54e25a90a21c999c1e2
Marcus <marcus.yks27@gmail.com>
2024-04-12 22:58:31 +0800

    MVP v2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-# ALLOWED_HOSTS=[".railway.app"]
+ALLOWED_HOSTS=[".railway.app"]
 
 # Application definition
 
